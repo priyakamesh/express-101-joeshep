c6836790197ade14a9f02d5ae84623126235eab2
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-27 10:25:37 -0500

    created routes modules and worked on it

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
 
+require('dotenv').config();//look for .env file
 const express = require('express');
 const app = express();
-
+//routes modules
+const animalRoutes = require('./routes/animals')
 //middleware are placed b/w request and a response (example: transform stream)
 const requestTime = (req,res,next)=>{//req,res and next objects are available for all the middlewares
   req.requestedTime = Date.now();
@@ -17,25 +19,12 @@ const requestTime = (req,res,next)=>{//req,res and next objects are available fo
 app.use(express.static(__dirname + '/public')) //tell express to add this to middleware
 
 app.use(requestTime)
-app.get('/monkeys', (req,res,next)=>{
-  console.log("Fetching some monkeys");
-  console.log(`This ran at ${req.requestedTime}`)
-  res.sendFile(__dirname + '/public/monkeys.html');
-})
-
-app.get('/chickens', (req,res,next)=>{
-  res.send(`<h3>No chickens for you</h3><form method="POST"><input type="text"><button type="submit">push</button></form>`)
-});
-
-app.post('/chickens',(req,res,next)=>{
-  console.log("Posting chicken request");
-});
-
+app.use(animalRoutes)
 app.use((req,res)=>{//last middleware in the stack
   res.send('no fred')
 })
 
-
-app.listen(8080,()=>{
-  console.log('server listening on port 8080')
+const port = process.env.PORT || 8080;
+app.listen(port,()=>{
+  console.log(`server listening on port ${port}`)
 });
---
+++ routes/animals.js
@@ -0,0 +1,28 @@
+const { Router } = require('express');
+const path = require('path');
+
+const animalRouter = Router();
+
+// animalRouter.use((req,res,next)=>{
+//   if(req.user === 'farmer') {
+//     next()
+//   } else {
+//     res.status(403).send('forbidden')
+//   }
+// })
+animalRouter.get('/monkeys', (req,res,next)=>{
+  console.log("Fetching some monkeys");
+  console.log(`This ran at ${req.requestedTime}`)
+  res.sendFile(path.join(__dirname,'../public' , '/monkeys.html'));//__dirname is the routes here
+})
+
+animalRouter.get('/chickens', (req,res,next)=>{
+  res.send(`<h3>No chickens for you</h3><form method="POST"><input type="text"><button type="submit">push</button></form>`)
+});
+
+animalRouter.post('/chickens',(req,res,next)=>{
+  console.log("Posting chicken request");
+});
+
+
+module.exports = animalRouter ;
