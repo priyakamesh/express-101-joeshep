fb19f6cb7a59f97328ca89a70ea0857104094eba
krishnapriya <krishnapriyakamesh@gmail.com>
2017-03-27 11:07:30 -0500

    added a second set of routes

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ require('dotenv').config();//look for .env file
 const express = require('express');
 const app = express();
 //routes modules
-const animalRoutes = require('./routes/animals')
+const animalRoutes = require('./routes/animals');
+const games = require('./routes/games');
 //middleware are placed b/w request and a response (example: transform stream)
 const requestTime = (req,res,next)=>{//req,res and next objects are available for all the middlewares
   req.requestedTime = Date.now();
@@ -20,6 +21,7 @@ app.use(express.static(__dirname + '/public')) //tell express to add this to mid
 
 app.use(requestTime)
 app.use(animalRoutes)
+app.use(games)
 app.use((req,res)=>{//last middleware in the stack
   res.send('no fred')
 })
---
+++ routes/games.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const path =require('path');
+
+const gameRouter = Router();
+
+gameRouter.get('/boardgames',(req,res,next)=>{
+  console.log('Fetching some boardgames');
+  res.sendFile(path.join(__dirname,'../public','boardGames.html'));;
+});
+
+gameRouter.get('/videogames',(req,res,next)=>{
+  console.log('Fetching videogames')
+  res.send(`<h3>Search for Games</h3><form method="POST"><input type="text"><button type="submit">push</button>`);
+});
+
+gameRouter.post('/videogames',(req,res,next)=>{
+  console.log("Submitted a form  request for videogames")
+});
+
+module.exports = gameRouter;
